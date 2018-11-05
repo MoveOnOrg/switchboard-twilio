55b2b4c88416e5a42bcc2a7185baeaf3c12c7f28
Jenny Cheng <jenny.cheng@moveon.org>
2018-11-05 15:31:58 -0600

    Improve documentation and fix indentation
    Also remove the word unicorn

--- cmds/buy.js
+++ cmds/buy.js
@@ -2,28 +2,28 @@ const ora = require('ora')
 const buyArgsValidation = require('../utils/buy_args_validation')
 
 module.exports = async (args) => {
-  const spinner = ora('Loading unicorns').start();
+  const spinner = ora('Loading').start();
 
   try {
-		const buyNumber = require('../utils/twilio')
+	const buyNumber = require('../utils/twilio')
 
     const areaCode = args.area_code || args.a
-		const quantity = args.quantity || args.q
+	const quantity = args.quantity || args.q
 
-		if (buyArgsValidation(areaCode, quantity)) {
-			spinner.text = `Buying ${quantity} phone numbers with area code ${areaCode}`
+	if (buyArgsValidation(areaCode, quantity)) {
+	  spinner.text = `Buying ${quantity} phone numbers with area code ${areaCode}`
 
-			Array.from(new Array(quantity),
-								 (x,i) => {
-									 spinner.text = `Buying phone number ${i+1}`
-									 const purchase = buyNumber('' + areaCode)
-									 return purchase
-								 })
+	  Array.from(new Array(quantity),
+				 (x,i) => {
+				   spinner.text = `Buying phone number ${i+1}`
+				   const purchase = buyNumber('' + areaCode)
+				   return purchase
+				 })
 
-			spinner.succeed(`Finished buying ${quantity} phone numbers with area code ${areaCode}`)
-		} else {
-			spinner.fail('Please ensure --area_code and --quantity are specified, and that --quantity is <= 100')
-		}
+	  spinner.succeed(`Finished buying ${quantity} phone numbers with area code ${areaCode}`)
+	} else {
+	  spinner.fail('Please ensure --area_code and --quantity are specified, and that --quantity is <= 100')
+	}
   } catch (err) {
     spinner.fail(err)
   }
--- utils/twilio.js
+++ utils/twilio.js
@@ -4,28 +4,27 @@ const authToken = process.env.AUTH_TOKEN
 const smsApplicationSid = process.env.SMS_APP_SID
 const client = require('twilio')(accountSid, authToken)
 
-
 module.exports = async (areaCode) => {
   // Find and then purchase a phone number
-	return client
-		.availablePhoneNumbers('US')
-		.local.list({
-			areaCode: areaCode,
-			smsEnabled: true,
+  return client
+	.availablePhoneNumbers('US')
+	.local.list({
+	  areaCode: areaCode,
+	  smsEnabled: true,
+	})
+	.then(data => {
+	  const number = data[0];
+	  return client.incomingPhoneNumbers.create({
+		friendlyName: 'Election Number',
+		phoneNumber: number.phoneNumber,
+		voiceMethod: 'POST',
+		voiceUrl: 'https://demo.twilio.com/welcome/voice/'
+	  })
+		.then(purchasedNumber => {
+		  client.messaging.services(smsApplicationSid)
+			.phoneNumbers
+			.create({phoneNumberSid: purchasedNumber.sid})
+			.then(num => console.log(num.phoneNumber))
 		})
-		.then(data => {
-			const number = data[0];
-			return client.incomingPhoneNumbers.create({
-				friendlyName: 'Election Number',
-				phoneNumber: number.phoneNumber,
-				voiceMethod: 'POST',
-				voiceUrl: 'https://demo.twilio.com/welcome/voice/'
-			})
-			.then(purchasedNumber => {
-				client.messaging.services(smsApplicationSid)
-					 .phoneNumbers
-					 .create({phoneNumberSid: purchasedNumber.sid})
-					 .then(num => console.log(num.phoneNumber))
-			})
-		})
+	})
 }
